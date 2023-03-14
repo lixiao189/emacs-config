5fcb35660d816cbd354c0b0fc3113d737d7fde9a
node189 <node189@icloud.com>
2023-03-14 18:58:46 +0800

    feat: add key bindings in evil mode

--- config/init-evil.el
+++ config/init-evil.el
@@ -6,22 +6,25 @@
   (setq evil-want-integration t) ;; This is optional since it's already set to t by default.
   (setq evil-want-keybinding nil)
   :config
-  (evil-mode 1))
+  (evil-mode 1)
+  (evil-set-leader 'normal (kbd "<SPC>")) ;; set leader key
+
+  ;; TODO keybinding for windows 
+  
+  ;; keybinding for file
+  (evil-define-key 'normal 'global (kbd "<leader>fs") 'save-buffer)
+  (evil-define-key 'normal 'global (kbd "<leader>ff") 'projectile--find-file)
+
+  ;; keybinding for buffer
+  (evil-define-key 'normal 'global (kbd "<leader>bs") 'switch-to-buffer)
+  (evil-define-key 'normal 'global (kbd "<leader>bk") 'kill-buffer)
+  )
 
 (use-package evil-collection
   :after evil
   :ensure t
   :config
   (evil-collection-init))
-
-(use-package evil-leader
-  :config
-  (global-evil-leader-mode)
-  (evil-leader/set-leader "<SPC>")
-  (evil-leader/set-key
-    "f" 'projectile-find-file
-    "b" 'switch-to-buffer
-    "k" 'kill-buffer))
 
 (provide 'init-evil)
 ;;; init-evil ends here
