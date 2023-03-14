6be698836ebd28f8dc86ea695e439bc2d2232985
node189 <node189@icloud.com>
2023-03-14 10:33:33 +0800

    improve for company mode

--- config/init-lsp.el
+++ config/init-lsp.el
@@ -2,18 +2,27 @@
 
 ;; completion front end
 (use-package company
-  :hook (after-init . global-company-mode))
+  :hook 
+  (after-init . global-company-mode)
+  :config 
+  (setq company-minimum-prefix-length 1) ; 只需敲 1 个字母就开始进行自动补全
+  (setq company-tooltip-align-annotations t)
+  (setq company-idle-delay 0.0)
+  (setq company-show-numbers t) ;; 给选项编号 (按快捷键 M-1、M-2 等等来进行选择).
+  (setq company-selection-wrap-around t)
+  (setq company-transformers '(company-sort-by-occurrence)))
 
 ;; lsp packages
 (use-package lsp-mode
   :init
   ;; set prefix for lsp-command-keymap (few alternatives - "C-l", "C-c l")
   (setq lsp-keymap-prefix "C-c l")
-  :hook (;; replace XXX-mode with concrete major-mode(e. g. python-mode)
-         ((python-mode 
-          c++-mode) . lsp)
-         ;; if you want which-key integration
-         (lsp-mode . lsp-enable-which-key-integration))
+  :hook (((
+        ;; add mode here
+        python-mode 
+        c++-mode
+        lisp-mode
+  ) . lsp) (lsp-mode . lsp-enable-which-key-integration))
   :commands lsp
   :config
   (setq read-process-output-max (* 1024 1024))
